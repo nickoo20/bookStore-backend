3d2588e48c73bbaba2e6566a0a3079cdb8cfa443
nickoo20 <gniyati20@gmail.com>
2024-05-20 16:58:04 +0530

    add config

---
+++ server.ts
@@ -0,0 +1,12 @@
+import app from "./src/app" ;
+import { config } from "./src/config/config";
+
+const startServer = () => {
+    const port=config.port || 3000 ;
+
+    app.listen(port, () => {
+        console.log(`Listening on Port ${port}!`) ;
+    })
+};
+
+startServer();  
